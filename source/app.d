addf498e3f783da260acaa342c18b0f00e59fedd
Christian Köstlin <christian.koestlin@esrlabs.com>
2017-02-24 21:01:02 +0100

    Fixed empty argument list

--- source/app.d
+++ source/app.d
@@ -4,6 +4,9 @@ import std.datetime;
 import consoled;
 
 int main(string[] args) {
+  if (args.length < 2) {
+    return 1;
+  }
   auto childCommand = args[1..$];
   auto cmd = escapeShellCommand(childCommand);
   auto sw = StopWatch(AutoStart.yes);
